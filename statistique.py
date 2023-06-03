263e847b1f0e38a7c7493a5ebd4700422699053d
ChrisChrisW <perso.christophewang@gmail.com>
2023-06-04 00:35:17 +0200

    update

--- statistique.py
+++ statistique.py
@@ -2,8 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
-from utils.printer import *
-
 def descriptive_statistics(file_path, num_classes):
     """
     Calcule les statistiques descriptives et affiche l'histogramme d'un fichier de données.
@@ -39,13 +37,13 @@ def descriptive_statistics(file_path, num_classes):
     print("Skewness :", skewness)
 
     # Affichage de l'histogramme
-    print_blue("\nAffichage de l'histogramme...")
+    print("\nAffichage de l'histogramme...")
     plt.hist(data, bins=num_classes, color='blue', alpha=0.7)
     plt.xlabel('Valeurs')
     plt.ylabel('Fréquence')
     plt.title('Histogramme des valeurs - ' + file_path)
     plt.grid(True)
-    plt.show()
+    plt.show(block=False)  # Ne pas bloquer l'exécution
 
 def inferential_statistics(file_path1, file_path2, alpha, alpha1, alpha2, mu0):
     """
@@ -87,7 +85,7 @@ def inferential_statistics(file_path1, file_path2, alpha, alpha1, alpha2, mu0):
     reject_h0_chi2 = p_value_chi2 < alpha2
 
     # Affichage des résultats
-    print_yellow("---- Statistiques inférentielles ----")
+    print("---- Statistiques inférentielles ----")
     print("Variable 1 -", file_path1)
     print("Moyenne :", mean1)
     print("Écart-type :", std_dev1)
@@ -95,21 +93,14 @@ def inferential_statistics(file_path1, file_path2, alpha, alpha1, alpha2, mu0):
     print("Moyenne :", mean2)
     print("Écart-type :", std_dev2)
     print("\nIntervalle de confiance pour la différence des moyennes (niveau {}%) :".format((1-alpha)*100), confidence_interval)
-    print_blue("\nRésultats du test de comparaison de la moyenne (H0: µ1 = µ2) :")
+    print("\nRésultats du test de comparaison de la moyenne (H0: µ1 = µ2) :")
     print("T-statistic :", t_statistic)
     print("P-value :", p_value)
     print("H0 rejetée :", reject_h0_mean)
-    print_blue("\nRésultats du test d'indépendance du chi-deux :")
+    print("\nRésultats du test d'indépendance du chi-deux :")
     print("P-value :", p_value_chi2)
     print("H0 rejetée :", reject_h0_chi2)
 
-# Afficher les statistiques descriptives et les histogrammes
-print_new_line()
 descriptive_statistics("notesproba.txt", num_classes=10)
-print_new_line()
 descriptive_statistics("notestat.txt", num_classes=10)
-print_new_line()
-
-# Effectuer les tests statistiques
 inferential_statistics("notesproba.txt", "notestat.txt", alpha=0.05, alpha1=0.05, alpha2=0.05, mu0=10.5)
-print_new_line()
